6e07388e54c5608d499be4a3fa998744381d3ade
Bruno Koga <koga@brunokoga.com>
2013-11-13 23:36:49 -0200

    Initial Commit

---
+++ Bubs/BKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BKAppDelegate.h
+//  Bubs
+//
+//  Created by Bruno Koga on 11/13/13.
+//  Copyright (c) 2013 Bruno Koga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Bubs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Bubs
+//
+//  Created by Bruno Koga on 11/13/13.
+//  Copyright (c) 2013 Bruno Koga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BKAppDelegate class]));
+    }
+}
