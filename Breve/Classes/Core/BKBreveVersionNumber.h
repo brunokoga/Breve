50faf3d8558ad8653dd45f9fc9dd022dcaf9d1eb
Bruno Koga <koga@brunokoga.com>
2013-11-29 13:07:02 -0200

    Version Number method

---
+++ Breve/Classes/Core/BKBreveVersionNumber.h
@@ -0,0 +1,15 @@
+//
+//  BKBreveVersionNumber.h
+//  Breve
+//
+//  Created by Bruno Koga on 11/29/13.
+//  Copyright (c) 2013 Bruno Koga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BKBreveVersionNumber : NSObject
+
++ (NSString *)versionNumber;
+
+@end
---
+++ Breve/Classes/Core/BKBreveVersionNumber.m
@@ -0,0 +1,26 @@
+//
+//  BKBreveVersionNumber.m
+//  Breve
+//
+//  Created by Bruno Koga on 11/29/13.
+//  Copyright (c) 2013 Bruno Koga. All rights reserved.
+//
+
+#import "BKBreveVersionNumber.h"
+
+@implementation BKBreveVersionNumber
+
++ (NSString *)versionNumber
+{
+    NSString *versionString = [[NSBundle mainBundle]
+                                   objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+#ifdef DEBUG
+    NSString *buildNumberString = [[NSBundle mainBundle]
+                                   objectForInfoDictionaryKey:@"CFBundleVersion"];
+    versionString = [versionString stringByAppendingFormat:@" (%@)", buildNumberString];
+#endif
+    
+    return versionString;
+}
+
+@end
