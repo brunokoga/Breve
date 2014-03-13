0ab1e1f059d2432abe4d9c8c5c7743d9bfd932c0
Bruno Koga <koga@brunokoga.com>
2014-03-13 10:48:14 -0300

    Tests: Basic Theme

---
+++ BrevesTests/BKBasicThemeTests.m
@@ -0,0 +1,48 @@
+//
+//  BKBasicThemeTests.m
+//  Breve
+//
+//  Created by Bruno Koga on 3/13/14.
+//  Copyright (c) 2014 Bruno Koga. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "BKBreveBasicTheme.h"
+
+@interface BKBasicThemeTests : XCTestCase
+@property (strong, nonatomic) BKBreveBasicTheme *theme;
+
+@end
+
+@implementation BKBasicThemeTests
+
+- (void)setUp
+{
+    [super setUp];
+  self.theme = [BKBreveBasicTheme new];
+}
+
+- (void)tearDown
+{
+  self.theme = nil;
+  [super tearDown];
+}
+
+- (void)testTheme
+{
+  XCTAssertEqual(UIStatusBarStyleDefault, [self.theme statusBarStyle], @"Wrong Status Bar Style");
+  
+  UIColor *expectedBackgroundColor = [UIColor whiteColor];
+  XCTAssertEqualObjects(expectedBackgroundColor, [self.theme backgroundColor], @"Wrong Color");
+  
+  UIColor *expectedForegroundColor = [UIColor blackColor];
+    XCTAssertEqualObjects(expectedForegroundColor, [self.theme foregroundColor], @"Wrong Color");
+  
+  UIColor *expectedTintColor = [UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0];
+    XCTAssertEqualObjects(expectedTintColor, [self.theme tintColor], @"Wrong Color");
+  
+  UIColor *expectedBackgroundAlternativeColor = [UIColor whiteColor];
+    XCTAssertEqualObjects(expectedBackgroundAlternativeColor, [self.theme backgroundAlternativeColor], @"Wrong Color");
+}
+
+@end
